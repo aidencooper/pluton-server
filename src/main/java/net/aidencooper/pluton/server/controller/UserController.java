56859f54b24e08914851954139bccd8d8b42b5ff
AidenCooper <aidencooper23@gmail.com>
2025-11-03 03:01:10 -0500

    Authorization server and .env file example

---
+++ src/main/java/net/aidencooper/pluton/server/controller/UserController.java
@@ -0,0 +1,16 @@
+package net.aidencooper.pluton.server.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+    @GetMapping
+    public String user(@AuthenticationPrincipal OAuth2User principal) {
+        return principal.getName();
+    }
+}
--- src/main/java/net/aidencooper/pluton/server/domain/User.java
+++ src/main/java/net/aidencooper/pluton/server/domain/User.java
@@ -25,6 +25,8 @@ public class User {
     @Column(nullable = false, unique = true)
     private String username;
 
+    private String password;
+
     @Column(nullable = false, unique = true)
     private String email;
 
